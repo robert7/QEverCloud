e411646dc0668474682e75c9433a50700770ea53
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2016-07-28 07:55:04 +0300

    Add unit test for Optional template class

---
+++ QEverCloud/src/tests/TestQEverCloud.cpp
@@ -0,0 +1,108 @@
+#include <QString>
+#include <QtTest>
+#include <QDebug>
+#include <QEverCloud.h>
+#include <utility>
+
+class TestEverCloudTest: public QObject
+{
+    Q_OBJECT
+public:
+    TestEverCloudTest();
+
+private Q_SLOTS:
+    void testOptional();
+};
+
+TestEverCloudTest::TestEverCloudTest()
+{}
+
+using namespace qevercloud;
+
+void TestEverCloudTest::testOptional()
+{
+    Optional<int> i;
+    QVERIFY(!i.isSet());
+
+    i = 10;
+    QVERIFY(i.isSet());
+    QVERIFY(i == 10);
+    i.clear();
+    QVERIFY(!i.isSet());
+
+    i.init().ref() = 11;
+    QVERIFY(i == 11);
+    static_cast<int&>(i) = 12;
+    QVERIFY(i == 12);
+
+    const Optional<int> ic = ' ';
+    QVERIFY(ic == 32);
+
+    i.clear();
+    i.init();
+    QVERIFY2(i.isSet() && i == int(), "i.isSet() && i == int()");
+
+    i.clear();
+    bool exception = false;
+    try {
+        qDebug() << i;
+    }
+    catch(const EverCloudException &) {
+        exception = true;
+    }
+    QVERIFY(exception);
+
+    Optional<int> y, k = 10;
+    y = k;
+    QVERIFY(y == 10);
+    Optional<double> d;
+    d = y;
+    QVERIFY(d == 10);
+    d = ' ';
+    QVERIFY(d == 32);
+
+    Optional<double> d2(y), d3(' '), d4(d);
+    QVERIFY(d2 == 10);
+    QVERIFY(d3 == 32);
+    QVERIFY(d4 == d);
+
+    Optional<int> oi;
+    Optional<double> od;
+    QVERIFY(oi.isEqual(od)); oi = 1;
+    QVERIFY(!oi.isEqual(od));
+    od = 1;
+    QVERIFY(oi.isEqual(od));
+    oi = 2;
+    QVERIFY(!oi.isEqual(od));
+
+    Note n1, n2;
+    QVERIFY(n1 == n2);
+    n1.guid = "12345";
+    QVERIFY(n1 != n2);
+    n2.guid = n1.guid;
+    QVERIFY(n1 == n2);
+
+#ifdef Q_COMPILER_RVALUE_REFS
+    Optional<int> oi1, oi2;
+    oi1 = 10;
+    oi2 = std::move(oi1);
+    QVERIFY(oi2 == 10);
+    QVERIFY(!oi1.isSet());
+
+    Note note1, note2;
+    note1.guid = "12345";
+    QVERIFY(note1.guid.isSet());
+    note2 = std::move(note1);
+    QVERIFY(note2.guid.isSet());
+    QVERIFY(!note1.guid.isSet());
+#endif
+
+    Optional<Timestamp> t;
+    t = 0;
+    t = Timestamp(0);
+    QVERIFY(t.ref() == Timestamp(0));
+}
+
+QTEST_MAIN(TestEverCloudTest)
+
+#include "TestQEverCloud.moc"
