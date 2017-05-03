575b6f33c49d83b7d777774cbbdc973519b2db9d
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2017-05-03 06:54:38 +0300

    Issue #1: add option to build the library without OAuth support and thus without related QtWebKit or QtWebEngine dependencies

--- QEverCloud/cmake/modules/QEverCloudFindQt5DependenciesWebEngineCore.cmake
+++ QEverCloud/cmake/modules/QEverCloudFindQt5DependenciesWebEngineCore.cmake
@@ -1,12 +1,3 @@
-if("${QEVERCLOUD_FIND_PACKAGE_ARG}" STREQUAL "")
-  set(QEVERCLOUD_FIND_PACKAGE_ARG "QUIET")
-else()
-  set(QEVERCLOUD_FIND_PACKAGE_ARG "")
-endif()
-
-QEverCloudFindPackageWrapper(Qt5Core ${QEVERCLOUD_FIND_PACKAGE_ARG})
-QEverCloudFindPackageWrapper(Qt5Network ${QEVERCLOUD_FIND_PACKAGE_ARG})
-QEverCloudFindPackageWrapper(Qt5Widgets ${QEVERCLOUD_FIND_PACKAGE_ARG})
 QEverCloudFindPackageWrapper(Qt5WebEngineCore ${QEVERCLOUD_FIND_PACKAGE_ARG})
 QEverCloudFindPackageWrapper(Qt5WebEngineWidgets ${QEVERCLOUD_FIND_PACKAGE_ARG})
 
--- QEverCloud/cmake/modules/QEverCloudFindQt5DependenciesWebKit.cmake
+++ QEverCloud/cmake/modules/QEverCloudFindQt5DependenciesWebKit.cmake
@@ -1,11 +1,2 @@
-if("${QEVERCLOUD_FIND_PACKAGE_ARG}" STREQUAL "")
-  set(QEVERCLOUD_FIND_PACKAGE_ARG "QUIET")
-else()
-  set(QEVERCLOUD_FIND_PACKAGE_ARG "")
-endif()
-
-QEverCloudFindPackageWrapper(Qt5Core ${QEVERCLOUD_FIND_PACKAGE_ARG})
-QEverCloudFindPackageWrapper(Qt5Network ${QEVERCLOUD_FIND_PACKAGE_ARG})
-QEverCloudFindPackageWrapper(Qt5Widgets ${QEVERCLOUD_FIND_PACKAGE_ARG})
 QEverCloudFindPackageWrapper(Qt5WebKit ${QEVERCLOUD_FIND_PACKAGE_ARG})
 QEverCloudFindPackageWrapper(Qt5WebKitWidgets ${QEVERCLOUD_FIND_PACKAGE_ARG})
