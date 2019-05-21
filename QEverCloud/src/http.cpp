7ffa99b58e6fe6a24dfdfa75c22077ac6d84d894
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2019-05-21 21:31:50 +0300

    Increase connection timeout to 180 seconds
    The default of 30 seconds doesn't appear to be enough for full sync in some cases

--- QEverCloud/src/http.cpp
+++ QEverCloud/src/http.cpp
@@ -1,6 +1,6 @@
 /**
  * Original work: Copyright (c) 2014 Sergey Skoblikov
- * Modified work: Copyright (c) 2015-2016 Dmitry Ivanov
+ * Modified work: Copyright (c) 2015-2019 Dmitry Ivanov
  *
  * This file is a part of QEverCloud project and is distributed under the terms of MIT license:
  * https://opensource.org/licenses/MIT
@@ -66,7 +66,7 @@ void ReplyFetcher::onDownloadProgress(qint64, qint64)
 
 void ReplyFetcher::checkForTimeout()
 {
-    const int connectionTimeout = 30*1000;
+    const int connectionTimeout = 180*1000;
     if ((QDateTime::currentMSecsSinceEpoch() - m_lastNetworkTime) > connectionTimeout) {
         setError(QStringLiteral("Connection timeout."));
     }
